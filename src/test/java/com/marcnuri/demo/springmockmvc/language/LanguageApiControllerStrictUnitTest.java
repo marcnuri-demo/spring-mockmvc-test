af235cdfd54cf9cb621695a236738904feecc3ef
Marc Nuri <marc@marcnuri.com>
2018-05-29 07:00:22 +0200

    Controller strict unit test

---
+++ src/test/java/com/marcnuri/demo/springmockmvc/language/LanguageApiControllerStrictUnitTest.java
@@ -0,0 +1,68 @@
+/*
+ * LanguageApiControllerStrictUnitTest.java
+ *
+ * Created on 2018-05-29, 6:46
+ */
+package com.marcnuri.demo.springmockmvc.language;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.doReturn;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.http.ResponseEntity;
+
+/**
+ * Created by Marc Nuri <marc@marcnuri.com> on 2018-05-29.
+ */
+public class LanguageApiControllerStrictUnitTest {
+
+//**************************************************************************************************
+//  Fields
+//**************************************************************************************************
+  private LanguageService languageService;
+  private LanguageApiController languageApiController;
+
+
+//**************************************************************************************************
+//  Test preparation
+//**************************************************************************************************
+  @Before
+  public void setUp() {
+    languageService = Mockito.mock(LanguageService.class);
+    languageApiController = new LanguageApiController(languageService);
+  }
+
+  @After
+  public void tearDown() {
+    languageApiController = null;
+    languageService = null;
+  }
+
+//**************************************************************************************************
+//  Tests
+//**************************************************************************************************
+  @Test
+  public void getLanguages_null_shouldReturnOk() throws Exception {
+    // Given
+    final String mockedEsoteric = "Arnoldc";
+    final List<String> mockedLanguages = Stream.concat(
+        LanguageRepository.LANGUAGES.stream(),
+        Stream.of(mockedEsoteric)).collect(Collectors.toList());
+    doReturn(mockedLanguages).when(languageService).getLanguages(null);
+
+    // When
+    final ResponseEntity<List<String>> result = languageApiController.getLanguages(null);
+
+    // Then
+    assertThat(result.getBody(), hasSize(mockedLanguages.size()));
+    assertThat(result.getBody(), hasItem(mockedEsoteric));
+  }
+}
