c3c3fc4e84a69b5865197e93146c27a1030823a1
Marc Nuri <marc@marcnuri.com>
2018-06-03 23:37:26 +0200

    Code coverage

---
+++ src/test/java/com/marcnuri/demo/springmockmvc/coffee/CoffeeRepositoryTest.java
@@ -0,0 +1,38 @@
+/*
+ * CoffeeRepositoryTest.java
+ *
+ * Created on 2018-06-03, 23:16
+ */
+package com.marcnuri.demo.springmockmvc.coffee;
+
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Created by Marc Nuri <marc@marcnuri.com> on 2018-06-03.
+ */
+@RunWith(JUnit4.class)
+public class CoffeeRepositoryTest {
+
+  @Test
+  public void findAll_na_shouldReturnList() {
+    // Given
+    final CoffeeRepository coffeeRepository = new CoffeeRepository();
+
+    // When
+    final List<String> results = coffeeRepository.findAll();
+
+    // Then
+    final int expectedSize = 10;
+    final String validCoffee = "Java";
+    assertThat(results, hasSize(expectedSize));
+    assertThat(results, hasItem(equalTo(validCoffee)));
+  }
+}
---
+++ src/test/java/com/marcnuri/demo/springmockmvc/coffee/CoffeeServiceTest.java
@@ -0,0 +1,64 @@
+/*
+ * CoffeeServiceTest.java
+ *
+ * Created on 2018-06-03, 23:25
+ */
+package com.marcnuri.demo.springmockmvc.coffee;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import static org.mockito.Mockito.doReturn;
+
+import com.marcnuri.demo.springmockmvc.coffee.CoffeeApiControllerWithContextTest.Config;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by Marc Nuri <marc@marcnuri.com> on 2018-06-03.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {
+    CoffeeService.class
+})
+@Import(Config.class)
+public class CoffeeServiceTest {
+
+  @Autowired
+  private CoffeeRepository coffeeRepository;
+
+  @Autowired
+  private CoffeeService coffeeService;
+
+  @Test
+  public void getCoffees_na_shouldReturnOk() throws Exception {
+    // Given
+    final String mockedCoffee = "Kopi luwak";
+    final List<String> mockedCoffees = Arrays.asList(mockedCoffee);
+    doReturn(mockedCoffees).when(coffeeRepository).findAll();
+
+    // When
+    final List<String> results = coffeeService.getCoffees();
+
+    // Then
+    assertThat(results, contains(mockedCoffee));
+  }
+
+  @Configuration
+  protected static class Config {
+
+    @Bean
+    public CoffeeRepository coffeeRepository() {
+      return Mockito.mock(CoffeeRepository.class);
+    }
+
+  }
+}
